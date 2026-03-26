786d4db47f8af82e7cb52c546721faaba579bb96
tejas-kumar-gh <dhimantejas1234@gmail.com>
2026-03-26 21:36:39 +0530

    xyz

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 // Enable CORS
 app.use(cors({
-  origin: 'http://localhost:5173', // Vite default port
+  origin: process.env.CLIENT_URL, // Vite default port
   credentials: true
 }));
 
