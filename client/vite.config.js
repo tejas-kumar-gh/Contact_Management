92fc435f29280142b85196d7dfbb59f52d25e9e3
tejas-kumar-gh <dhimantejas1234@gmail.com>
2026-03-26 21:46:51 +0530

    changes in frontend

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: import.meta.env.VITE_API_URL,
         changeOrigin: true,
       }
     }
